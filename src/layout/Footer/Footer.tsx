307c9ab655268dd858ab5e89b772816d88f3efeb
Rahul Dev Choudhari <irhymecodes@gmail.com>
2024-04-24 01:57:54 +0530

    feature: transaction page frontend

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="absolute z-50 left-0 right-0 bottom-0 h-[50px] rounded-b-[2rem] backdrop-blur-xl bg-gradient-to-r from-white/10 to-white/10">
+    <div className="absolute z-50 left-0 right-0 bottom-0 h-[65px] rounded-b-[2rem] backdrop-blur-xl bg-gradient-to-r from-white/10 to-white/10">
         <div className="w-[50%] mx-auto h-full flex justify-between items-center">
         <Link to="/sendEth">
             <FaEthereum className="text-2xl hover:text-white/30"/>
---
+++ src/pages/transaction/Balance.tsx
@@ -0,0 +1,26 @@
+import { useContext } from "react"
+import { WalletContext } from "../../context/WalletContext"
+
+const Balance = () => {
+
+    const {wallet} = useContext(WalletContext)
+    console.log(wallet)
+
+  return (
+    <div className=" flex flex-col justify-center items-center">
+        <h1 className="text-md">Current Wallet Balance</h1>
+        <h1 className="text-[5rem]">${wallet.balance}</h1>
+        <div className="cursor-pointer relative z-50 flex gap-3 justify-center items-center rounded-xl py-2 px-4 backdrop-blur-xl bg-gradient-to-r from-white/10 to-white/10">
+            {/* Element extra */}
+          <div className="w-[10px] h-[10px] rounded-full bg-[#FDD401]"></div>                
+
+            <h1 className="">{wallet?.accounts[0]?.substring(0, 6)}...</h1>
+
+            <div className="absolute left-4 blur-md w-[10px] h-[10px] bg-[#FDD401] rounded-full "></div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Balance
